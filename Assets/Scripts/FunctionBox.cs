e082406e4970b942bdfcbfcc72e999d4b60f01db
eduardo <eduregis1@gmail.com>
2025-03-19 18:29:30 -0300

    creating prefabs to function and conditional boxes

---
+++ Assets/Scripts/FunctionBox.cs
@@ -0,0 +1,20 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FunctionBox : MonoBehaviour
+{
+    public Button button;
+    public TextMeshProUGUI title;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
