f2410264890ce3b04797af503fed9a0b2405e01a
eduardo <eduregis1@gmail.com>
2025-04-06 23:07:42 -0300

    home scene created

---
+++ Assets/Scripts/Controllers/ButtonController.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Button))]
+public class ButtonController : MonoBehaviour
+{
+    public enum ActionType { LoadScene, CustomAction }
+    
+    [Header("Configuration")]
+    public ActionType actionType = ActionType.LoadScene;
+    public string sceneName;
+    public UnityEvent customAction;
+
+    private void Awake()
+    {
+        Button button = GetComponent<Button>();
+        button.onClick.AddListener(OnButtonClicked);
+    }
+
+    private void OnButtonClicked()
+    {
+        GetComponent<Button>().interactable = false;
+        
+        switch(actionType)
+        {
+            case ActionType.LoadScene:
+                if (!string.IsNullOrEmpty(sceneName))
+                {
+                    SceneManager.LoadScene(sceneName);
+                }
+                else
+                {
+                    Debug.LogError("Scene name not set!");
+                }
+                break;
+                
+            case ActionType.CustomAction:
+                customAction.Invoke();
+                break;
+        }
+    }
+}
